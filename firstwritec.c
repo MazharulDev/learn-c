9013f0f60b0c03c57965e223535750a55ad937b5
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-10-28 09:32:38 +0600

    int float char

--- firstwritec.c
+++ firstwritec.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-    printf("Hello, world!\n");
-    printf("have a nice day");
+    // printf("Hello, world!\n\n"); // \n for new line
+    // printf("have a nice day\t");
+    // printf("\"new line is this\" \t");
+    // printf("new line is this");
+    int a = 1;
+    float b = 4.25;
+    char c = 'M';
+
+    printf("%d\n", a);
+    printf("%f\n", b);
+    printf("%c", c);
     return 0;
 }
