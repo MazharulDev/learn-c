9af16192a62a20b51b6f0a391a21fe5476111c85
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2024-10-29 08:19:19 +0600

    c data type of variable

--- firstwritec.c
+++ firstwritec.c
@@ -2,16 +2,69 @@
 
 int main()
 {
+    // text formatting
+
     // printf("Hello, world!\n\n"); // \n for new line
     // printf("have a nice day\t");
     // printf("\"new line is this\" \t");
     // printf("new line is this");
-    int a = 1;
-    float b = 4.25;
-    char c = 'M';
 
-    printf("%d\n", a);
-    printf("%f\n", b);
-    printf("%c", c);
+    // variable declaration and initialization
+
+    // int a = 1;
+    // float b = 4.25;
+    // char c = 'M';
+
+    // printf("%d\n", a);
+    // printf("%f\n", b);
+    // printf("%c", c);
+
+    // int a = 7;
+    // float b = 3.5;
+    // char c = 'T';
+
+    // int a;
+    // a = 5;
+
+    // printf("My favorite number is: %d", a);
+
+    // int a = 4;
+    // int b = 3;
+    // int sum = a + b;
+    // printf("sum result is: %d", sum);
+
+    // int x = 3, y = 2, z = 1;
+    // printf("sum result is: %d", x + y + z);
+
+    // int x, y, z;
+    // x = y = z = 50;
+    // printf("sum result is: %d", x + y + z);
+
+    // int a = 5;
+    // int b = 3;
+    // int c;
+    // c = a * b;
+    // printf("sum result is: %i", c);
+
+    // char myName[] = "Mazharul";
+    // printf("My name is: %s", myName);
+
+    // float x = 35e3;
+    // double y = 12E4;
+    // printf("%f\n", x);
+    // printf("%lf", y);
+
+    // float a = 2.5;
+    // printf("%.2f", a);
+
+    int items = 50;
+    float cost_per_item = 2.22;
+    float total_cost = items * cost_per_item;
+    char currency = '$';
+    printf("number of items: %d\n", items);
+    printf("Cost per Item: %c%.2f\n", currency, cost_per_item);
+
+    printf("Total cost: %c%.2f\n", currency, total_cost);
+
     return 0;
 }
